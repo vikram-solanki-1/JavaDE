ca1a456bbe6110651a6819a0c0cc2df03201a941
Vikram Solanki <134689573+vikram-solanki-1@users.noreply.github.com>
2023-07-10 17:09:29 +0530

    Create 4. array.java

---
+++ 4. array.java
@@ -0,0 +1,16 @@
+package com.vikram_package;
+class E1
+{
+    public static void main (String a[]) {
+        int [] array1 = {1,2,3,4,5};
+        System.out.println(array1[4]);
+
+        int [] [] multiarray1= {
+                {1,2},
+                {3,4},
+                {5,6},
+                {7}
+                };
+        System.out.println(multiarray1[0].length);
+    }
+}
